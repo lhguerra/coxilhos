ec67fd8a786d161be8d0004399b91db60bdfa0f6
lhguerra <lhguerra@gmail.com>
2014-04-01 19:42:48 -0300

    added functional scripts

--- js/main.js
+++ js/main.js
@@ -12,3 +12,34 @@ $(function() {
         target: '+=1'
     });
 });
+
+// Form Scripts
+;(function ($) {
+  "use strict";
+
+  var $form = $('#ss-form');
+
+  /**
+   * Callback for form submittion.
+   */
+  function afterSubmittion() {
+    $form.addClass('submitted');
+  }
+
+  /**
+   * Listen to form submition event.
+   */
+  $form.ajaxForm({
+    success: afterSubmittion,
+    error: afterSubmittion
+  });
+
+}(jQuery));
+
+// Animation for 'learn more' transition
+jQuery(document).ready(function() {
+  $(".scroll").click(function(event){   
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
+  });
+});
